939893576c454da9478a91e51bfc3cf04a8d4f26
Assanali Tungat <ali@192.168.10.2>
2025-01-09 01:05:35 +0500

    database ts to js. split drizzle and turso

--- app/database/drizzle.js
+++ app/database/drizzle.js
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-import dotenv from "dotenv";
+import { turso } from "./turso";
 
 import * as authTableQ from "./schema/auth.drizzle";
 import * as businessTableQ from "./schema/business.drizzle";
@@ -10,13 +9,6 @@ import * as soxTableQ from "./schema/sox.drizzle";
 import * as taskTableQ from "./schema/task.drizzle";
 import * as docsTableQ from "./schema/docs.drizzle";
 import * as rbacTableQ from "./schema/rbac.drizzle";
-
-dotenv.config();
-
-const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
-});
 
 export const db = drizzle(turso, {
   schema: {
