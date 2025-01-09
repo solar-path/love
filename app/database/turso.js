4e97083e3fc2e94809958d62da858f5506c02bec
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2025-01-09 11:53:17 +0500

    debug .env issue for vite

--- app/database/turso.js
+++ app/database/turso.js
@@ -1,8 +1,6 @@
 import { createClient } from "@libsql/client";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: import.meta.env.VITE_TURSO_DATABASE_URL,
+  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN,
 });
--- app/routes/admin/crm/inquiry/inquiry.page.jsx
+++ app/routes/admin/crm/inquiry/inquiry.page.jsx
@@ -1,6 +1,6 @@
 import { turso } from "@database/turso";
 
-const inquiryList = await turso.execute(sql`SELECT * FROM crm_inquiry`);
+const inquiryList = await turso.execute(`SELECT * FROM crm_inquiry`);
 
 export default function InquiryPage() {
   console.log(inquiryList);
