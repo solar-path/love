4fed214782b0cfd7a71636ece2384da0e2823229
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2025-01-09 17:41:20 +0500

    migration works

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-
 export default defineConfig({
   out: './src/database/migrations',
   schema: './src/database/schema/*.drizzle.ts',
   dialect: 'turso',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_DATABASE_AUTH_TOKEN!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
