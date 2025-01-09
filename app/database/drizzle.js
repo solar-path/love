6f75fecad704c7d966ca58ec36cfbec73970869a
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2025-01-09 14:22:03 +0500

    remove turso.js

--- app/database/drizzle.js
+++ app/database/drizzle.js
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { turso } from "./turso";
+import { createClient } from "@libsql/client";
 
 import * as authTableQ from "./schema/auth.drizzle";
 import * as businessTableQ from "./schema/business.drizzle";
@@ -9,6 +9,11 @@ import * as soxTableQ from "./schema/sox.drizzle";
 import * as taskTableQ from "./schema/task.drizzle";
 import * as docsTableQ from "./schema/docs.drizzle";
 import * as rbacTableQ from "./schema/rbac.drizzle";
+
+export const turso = createClient({
+  url: import.meta.env.VITE_TURSO_DATABASE_URL,
+  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN,
+});
 
 export const db = drizzle(turso, {
   schema: {
--- app/database/turso.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@libsql/client";
-
-export const turso = createClient({
-  url: import.meta.env.VITE_TURSO_DATABASE_URL,
-  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN,
-});
