cd7284f8be7ad9084fdbb57aca794f285a285601
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2025-01-10 14:17:46 +0500

    respond on error

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,40 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import type { ErrorResponse } from "./helper/types";
+import { HTTPException } from "hono/http-exception";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errResponse =
+      err.res ??
+      c.json<ErrorResponse>(
+        {
+          success: false,
+          error: err.message,
+          isFormError:
+            err.cause && typeof err.cause === "object" && "form" in err.cause
+              ? err.cause.form === true
+              : false,
+        },
+        err.status
+      );
+    return errResponse;
+  }
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : err.stack ?? err.message,
+    },
+    500
+  );
+});
+
+export default app;
