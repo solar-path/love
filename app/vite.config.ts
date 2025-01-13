83dc378e5fb7136b9c0cae5bda8e94802c6f725c
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2025-01-13 14:46:22 +0500

    setup proxy on app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
