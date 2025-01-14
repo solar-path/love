603998b9d144ecff3ae91658d999d4d12b79987c
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2025-01-14 17:04:44 +0500

    QHeader - apply tailwind to fix dancing routes

--- app/src/components/QHeader.ui.tsx
+++ app/src/components/QHeader.ui.tsx
@@ -24,98 +24,98 @@ export default function QHeader() {
 
   if (!isAuthenticated.value) {
     return (
-      <Navbar fluid rounded>
-        <Navbar.Brand as={Link} to="/">
+      <Navbar fluid rounded className="flex items-center justify-between">
+        <Navbar.Brand as={Link} to="/" className="flex items-center">
           <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Aneko Logo" />
         </Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link as={Link} to="/documentation">
-            Documentation
-          </Navbar.Link>
-
-          <Navbar.Link as={Link} to="/pricing">
-            Pricing
-          </Navbar.Link>
-
-          <Navbar.Link
-            onClick={() => fillDrawer(<RegisterForm />, "Sign up")}
-            className="text-red-700 font-bold text-lg"
-          >
-            Sign up
-          </Navbar.Link>
-          <Navbar.Link onClick={() => fillDrawer(<LoginForm />, "Sign in")}>
-            <Button size="sm" color="dark">
-              Sign in
-            </Button>
-          </Navbar.Link>
+        <Navbar.Collapse className="flex items-center gap-4 md:flex-row">
+          <div className="flex items-center gap-4">
+            <Navbar.Link as={Link} to="/documentation">
+              Documentation
+            </Navbar.Link>
+            <Navbar.Link as={Link} to="/pricing">
+              Pricing
+            </Navbar.Link>
+            <Navbar.Link
+              onClick={() => fillDrawer(<RegisterForm />, "Sign up")}
+              className="text-red-700 font-bold text-lg"
+            >
+              Sign up
+            </Navbar.Link>
+            <Navbar.Link onClick={() => fillDrawer(<LoginForm />, "Sign in")}>
+              <Button size="sm" color="dark">
+                Sign in
+              </Button>
+            </Navbar.Link>
+          </div>
         </Navbar.Collapse>
       </Navbar>
     );
   } else {
     return (
-      <Navbar fluid rounded>
-        <Navbar.Brand>company selector</Navbar.Brand>
+      <Navbar fluid rounded className="flex items-center justify-between">
+        <Navbar.Brand className="flex items-center">
+          company selector
+        </Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link as={Link} to="/documentation">
-            Documentation
-          </Navbar.Link>
-          <Navbar.Link as={Link} to="/admin">
-            Administation panel
-          </Navbar.Link>
-          <Navbar.Link as={Link} to="/protected">
-            Dashboard
-          </Navbar.Link>
+        <Navbar.Collapse className="flex items-center gap-4 md:flex-row">
+          <div className="flex items-center gap-4">
+            <Navbar.Link as={Link} to="/documentation">
+              Documentation
+            </Navbar.Link>
+            <Navbar.Link as={Link} to="/admin">
+              Administation panel
+            </Navbar.Link>
+            <Navbar.Link as={Link} to="/company">
+              Dashboard
+            </Navbar.Link>
 
-          <Navbar.Link>
-            <Dropdown label="Services" color="dark">
-              {serviceItems.map((item) => (
+            <Navbar.Link>
+              <Dropdown label="Services" color="dark">
+                {serviceItems.map((item) => (
+                  <Dropdown.Item>
+                    <Link to={item.href} className="flex items-center">
+                      <item.icon size={16} className="mr-2 h-5 w-5" />
+                      {item.name}
+                    </Link>
+                  </Dropdown.Item>
+                ))}
+              </Dropdown>
+            </Navbar.Link>
+
+            <Navbar.Link>
+              <Dropdown
+                label={
+                  currentUser.value?.avatar ? (
+                    <Avatar
+                      alt="User settings"
+                      img={currentUser.value.avatar}
+                      rounded
+                    />
+                  ) : (
+                    <Avatar alt="User settings" rounded />
+                  )
+                }
+                arrowIcon={false}
+                inline
+              >
+                <Dropdown.Header>
+                  <span className="block text-sm">
+                    {currentUser.value.fullname}
+                  </span>
+                  <span className="block truncate text-sm font-medium">
+                    {currentUser.value.email}
+                  </span>
+                </Dropdown.Header>
                 <Dropdown.Item>
-                  <Link
-                    to={item.href}
-                    className="flex items-center justify-start"
-                  >
-                    <item.icon size={16} className="mr-2 h-5 w-5" />
-                    {item.name}
-                  </Link>
+                  <Link href="/company/profile">Profile</Link>
                 </Dropdown.Item>
-              ))}
-            </Dropdown>
-          </Navbar.Link>
-
-          <Navbar.Link>
-            <Dropdown
-              label={
-                currentUser.value?.avatar ? (
-                  <Avatar
-                    alt="User settings"
-                    img={currentUser.value.avatar}
-                    rounded
-                  />
-                ) : (
-                  <Avatar alt="User settings" rounded />
-                )
-              }
-              arrowIcon={false}
-              inline
-              className="w-auto"
-            >
-              <Dropdown.Header>
-                <span className="block text-sm">
-                  {currentUser.value.fullname || currentUser.value.email}
-                </span>
-                <span className="block truncate text-sm font-medium">
-                  {currentUser.value.email}
-                </span>
-              </Dropdown.Header>
-              <Dropdown.Item>
-                <Link href="/company/profile">Profile</Link>
-              </Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item onClick={() => logout()}>Sign out</Dropdown.Item>
-            </Dropdown>
-          </Navbar.Link>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={() => logout()}>Sign out</Dropdown.Item>
+              </Dropdown>
+            </Navbar.Link>
+          </div>
         </Navbar.Collapse>
       </Navbar>
     );
